709d8b9f32ab2f873c291fed683aa02fb133cc7b
rabbaniqibar2302 <rizkiakbar2302@gmail.com>
2023-10-09 08:40:11 +0700

    hensin

--- config.php
+++ config.php
@@ -1,4 +1,41 @@
 <?php
 // Connect To Database
-$host = mysqli_connect("localhost","root",null,"crud");
+$host = mysqli_connect("localhost", "root", null, "crud");
+
+
+
+if (isset($_POST['tambah'])) {
+    // Variabel
+    $name = $_POST['nama'];
+    $email = $_POST['email'];
+    $no_hape = $_POST['no_hape'];
+
+    // Perintah ke dalam tabel
+    $tambah = mysqli_query($host, "insert into user values(
+    null,
+    '$name',
+    '$email',
+    '$no_hape'
+    )");
+
+    // Notifikasi
+    if ($tambah) {
+?>
+        <script>
+            alert("Data Berhasil Di Tambahkan");
+            window.location.href = "index.php";
+        </script>
+    <?php
+    } else {
+    ?>
+        <script>
+            alert("Data Tidak Berhasil Di Tambahkan");
+            window.location.href = "add.php";
+        </script>
+<?php
+    }
+}
+
+
+
 ?>
