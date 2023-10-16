d80cb82643f96bc2dc3c565b3cc863e22bb75420
rabbaniqibar2302 <rizkiakbar2302@gmail.com>
2023-10-16 11:04:25 +0700

    masih error show data, fix later

--- CRUD OOP/config.php
+++ CRUD OOP/config.php
@@ -1,19 +1,32 @@
 <?php
 
 class db{
+    var $query;
     var $host = "localhost";
     var $name = "root";
     var $pass = null;
     var $db = "crud";
 
     function __construct(){
-        mysqli_connect(
+        $this->query = mysqli_connect(
             $this->host,
             $this->name,
             $this->pass,
             $this->db
         );
     }
+
+    function show_data(){
+        $query = self::$query;;
+        $show = mysqli_query($query,"select * from user_oop");
+        while($data = mysqli_fetch_array($show)){
+            $hasil[] = $data;
+        }
+        return $hasil;
+
+    }
+
+    
 
 }
 
--- CRUD OOP/index.php
+++ CRUD OOP/index.php
@@ -1,17 +1,35 @@
 <html>
-    <head>
-        <title>CRUD OOP</title>
-    </head>
-    <body>
-        <h1>Data User</h1>
-        <a href="">Tambah Data</a>
-        <table border="1">
+
+<head>
+    <title>CRUD OOP</title>
+</head>
+
+<body>
+    <h1>Data User</h1>
+    <a href="">Tambah Data</a>
+    <table border="1">
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>Alamat</th>
+            <th>Usia</th>
+        </tr>
+        <?php
+        include 'config.php';
+        $db = new db();
+        $no = 1;
+        foreach($db->show_data() as $data){
+            echo "
             <tr>
-                <th>No</th>
-                <th>Nama</th>
-                <th>Alamat</th>
-                <th>Usia</th>
+            <td>.$no++.</td>
+            <td>{$data['nama']}</td>
+            <td>{$data['email']}</td>
+            <td>{$data['no_hape']}</td>
             </tr>
-        </table>
-    </body>
+            ";
+        }
+        ?>
+    </table>
+</body>
+
 </html>
